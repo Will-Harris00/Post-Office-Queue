5fe532a010a67ecea424fe2b05bf34af661e804c
Will-Harris00 <wjharris00@outlook.com>
2021-02-20 20:02:51 +0000

    Header formatting for readInputFile

--- readInputFile.h
+++ readInputFile.h
@@ -6,7 +6,7 @@
 
 /*----- function prototypes --------------------------------------------------*/
 
-int readInputFile(char*, int *, unsigned int *, unsigned int *, unsigned int *, unsigned int *, unsigned int *);
-int validateParameters(int *, unsigned int *, unsigned int *, unsigned int *, unsigned int *);
+int readInputFile(char*, int*, unsigned int*, unsigned int*, unsigned int*, unsigned int*, unsigned int*);
+int validateParameters(int*, unsigned int*, unsigned int*, unsigned int*, unsigned int*);
 
 #endif
